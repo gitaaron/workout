fd588a4043c86dfca73dac84136949291b64dc4d
Aaron Surty <aaron.surty@gmail.com>
2015-04-27 10:57:26 -0700

    simple workout api

--- webapp/app.js
+++ webapp/app.js
@@ -1,7 +1,5 @@
 var express = require('express');
 
-
-var express = require('express');
 var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
@@ -9,6 +7,20 @@ app.get('/', function(req, res) {
     res.send('hello world');
 });
 
+var rep_count = 0;
+
+app.get('/workout', function(req, res) {
+    res.end(JSON.stringify({count:rep_count}));
+});
+
+app.get('/workout/reps/clear', function(req, res) {
+    rep_count = 0;
+    res.end(JSON.stringify({count:rep_count}));
+});
+
+app.get('/workout/reps/increment', function(req, res) {
+    res.end(JSON.stringify({count:++rep_count}));
+});
 
 app.listen(9099);
 
